6f8d0f85dfffc092f6226ce006f4e0a1068535cb
CodingTobi <tobi.sassin@gmail.com>
2020-06-28 21:31:41 +0200

    Noch mehr einrückungen und hab die Methode 'getCount()' ersetzt

--- Trie.java
+++ Trie.java
@@ -1,3 +1,5 @@
+package de.tobiassassin.ADS.Uebungsblatt10.abgabe2; //gitignore -> nur für EclipseHast 
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -16,7 +18,8 @@ public class Trie {
 			this.value = c;
 		}
 
-		/* Fügt den Buchstaben c in den Suchbaum des TrieNodes ein. */ public void add(char c, int count) {
+		/* Fügt den Buchstaben c in den Suchbaum des TrieNodes ein. */
+		public void add(char c, int count) {
 			st.inc(c, count);
 			if (st.getCount(c) > maxChildCount) {
 				maxChildCount = st.getCount(c);
@@ -24,7 +27,8 @@ public class Trie {
 			}
 		}
 
-		/* Liefert den zu c gehörigen TrieNode zurück */ public TrieNode get(char c) {
+		/* Liefert den zu c gehörigen TrieNode zurück */
+		public TrieNode get(char c) {
 			return st.getNode(c);
 		}
 	}
@@ -51,7 +55,8 @@ public class Trie {
 		}
 
 		/* Erhöht den Zähler des Schlüssels um count. Falls der Schlüssel noch nicht vorhanden ist, wird 
-		ein neuer Knoten mit Zähler count eingeführt. */ public void inc(char key, int count) {
+		ein neuer Knoten mit Zähler count eingeführt. */
+		public void inc(char key, int count) {
 			if (root == null) {
 				root = new Node(key, count);
 				return;
@@ -75,24 +80,23 @@ public class Trie {
 			current.count += count;
 		}
 
-		/* Liefert den Zähler des Schlüssels key zurück */ public long getCount(char key) {
+		/* Liefert den Zähler des Schlüssels key zurück */
+		public long getCount(char key) {
 			if (root == null)
 				return 0L;
+
 			// es wird entweder gefunden oder 0 zurueckgegeben
-			Node current = root;
-			while (current.key != key) {
-				if (key < current.key) {
-					current = current.left;
-				} else {
-					current = current.right;
-				}
-				if (current == null)
+			var tempN = root;
+			while (tempN.key != key) {
+				tempN = (key < tempN.key) ? tempN.left : tempN.right;
+				if (tempN == null)
 					return 0L;
 			}
-			return current.count;
+			return tempN.count;
 		}
 
-		/* Liefert das (TrieNode-)Kind für key zurück */ public TrieNode getNode(char key) {
+		/* Liefert das (TrieNode-)Kind für key zurück */
+		public TrieNode getNode(char key) {
 			if (root == null)
 				return null;
 			Node current = root;
@@ -117,7 +121,8 @@ public class Trie {
 		root = new TrieNode('*');
 	}
 
-	/* Fügt einen Trainingsquery zum Trie hinzu */ public void add(String s, int count) {
+	/* Fügt einen Trainingsquery zum Trie hinzu */
+	public void add(String s, int count) {
 		TrieNode current = root;
 		for (char c : s.toCharArray()) {
 			current.add(c, count);
@@ -127,7 +132,8 @@ public class Trie {
 	}
 
 	/* Sagt für den Präfix eines Queries den vollständigen Queries voraus. Ist ein Präfix nicht im Baum 
-	vorhanden, wird null zurückgegeben. */ public String predict(String prefix) {
+	vorhanden, wird null zurückgegeben. */
+	public String predict(String prefix) {
 		StringBuilder result = new StringBuilder();
 		TrieNode current = root;
 		for (int i = 0; i < prefix.length(); i++) {
@@ -168,18 +174,36 @@ public class Trie {
 		}
 	}
 
-	public static void main(String[] args) throws IOException { /*Trie t = new Trie(); t.eval(); 
-																System.out.println("trump : " + t.predict("trump")); System.out.println("german : " + 
-																t.predict("german")); System.out.println("mo : " + t.predict("mo")); System.out.println("paw : " 
-																+ t.predict("paw")); System.out.println("secret : " + t.predict("secret")); 
-																System.out.println("best : " + t.predict("best")); System.out.println("pro : " + 
-																t.predict("pro")); System.out.println("small : " + t.predict("small")); System.out.println("snow 
-																: " + t.predict("snow"));
-																System.out.println("soc : " + t.predict("soc")); TrieNode tn1 = new TrieNode(); TrieNode tn0 = 
-																new TrieNode('*'); tn0.add('b', 10); tn0.add('u', 10); tn0.add('k', 20); tn0.add('o', 10); 
-																tn0.add('v', 30); Trie t = new Trie(); t.add("abc", 1); t.add("abd", 1); t.add("aba", 5); 
-																t.add("abf", 10); t.add("abaf", 20); String result = t.predict("ab"); 
-																System.out.println(result);*/
+	public static void main(String[] args) throws IOException {
+		/*
+		Trie t = new Trie();
+		t.eval();
+		System.out.println("trump : " + t.predict("trump"));
+		System.out.println("german : " + t.predict("german"));
+		System.out.println("mo : " + t.predict("mo"));
+		System.out.println("paw : " + t.predict("paw"));
+		System.out.println("secret : " + t.predict("secret"));
+		System.out.println("best : " + t.predict("best"));
+		System.out.println("pro : " + t.predict("pro"));
+		System.out.println("small : " + t.predict("small"));
+		System.out.println("snow : " + t.predict("snow"));
+		System.out.println("soc : " + t.predict("soc"));
+		TrieNode tn1 = new TrieNode();
+		TrieNode tn0 = new TrieNode('*');
+		tn0.add('b', 10);
+		tn0.add('u', 10);
+		tn0.add('k', 20);
+		tn0.add('o', 10);
+		tn0.add('v', 30);
+		Trie t = new Trie();
+		t.add("abc", 1);
+		t.add("abd", 1);
+		t.add("aba", 5);
+		t.add("abf", 10);
+		t.add("abaf", 20);
+		String result = t.predict("ab");
+		System.out.println(result);
+		*/
 	}
 
 }
